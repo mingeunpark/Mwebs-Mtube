7d7965a0bc1355166a95376fbeb67858dbb170f1
MinGeun <sksjge9222@gmail.com>
2024-01-12 13:48:25 +0900

    21.2_채널상세페이지_API소스처리완료시_화면출력

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchFromAPI } from '../utils/api'
@@ -12,46 +11,54 @@ import { CiRead } from "react-icons/ci";
 const Channel = () => {
     const { channelID } = useParams();
     const [ channelDetail, setChannelDetail ] = useState();
+    const [ loading, setLoading ] = useState(true);
 
     useEffect(() => {
         const fetchResults = async () => {
+            try {
             const data = await fetchFromAPI(`channels?part=snippet,statistics,contentDetails,brandingSettings&id=${channelID}`);
             setChannelDetail(data.items[0]);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
         };
         fetchResults();
     }, [channelID]);
+
+    const channelPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main 
             title = "유튜브 채널"
             description="유튜브 채널페이지입니다.">
             
-            {channelDetail && (
-                <section id='channel'>
+            <section id='channel' className={channelPageClass}>
+                {channelDetail && (
+                    <div className='channel__inner'>
 
 
-                    <div className='channel__header' style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
-                        <div className='circle'>
-                            <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
-                        </div>
-                    </div>
-
-
-                    <div className='channel__info'>
-                        <h3 className='title'>{channelDetail.snippet.title}</h3>
-                        <p className='desc'>{channelDetail.snippet.description}</p>
-
-
-                        <div className='info'>
-                            <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
-                            <span><CiMedal />{channelDetail.statistics.videoCount}</span>
-                            <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                        <div className='channel__header' style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                            <div className='circle'>
+                                <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                            </div>
                         </div>
 
-                        
+                        <div className='channel__info'>
+                            <h3 className='title'>{channelDetail.snippet.title}</h3>
+                            <p className='desc'>{channelDetail.snippet.description}</p>
+
+                            <div className='info'>
+                                <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                            </div>
+
+                        </div>
                     </div>
-                </section>
             )}
+            </section>
         </Main>
     )
 }
